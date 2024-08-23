1eb902509ec3cca9119b8264003f509c0c3da971
白石 <572268633@qq.com>
2024-08-23 23:31:38 +0800

    update: 更新响应模板

--- src/static/js/response.js
+++ src/static/js/response.js
@@ -12,45 +12,35 @@ function addInputArea(id) {
         case 'fields-list': {
             let area = document.getElementById(id);
             console.log("add: fields_list")
-            // 随机8位id 0-9 a-z A-Z
             let tmpId = randomId('field-');
             let temp = `<div id="${tmpId}" class="response-field_item">
                             <label class="response-field-label">名称: </label>
-                            <input class="input-data response-field-input" id='field_name' />&emsp; 
+                            <input class="input-data response-field-input" id='field_name' /> &emsp;
                             <label class="response-field-label">类型: </label>
-                            <input class="input-data response-field-input" id='field_type' />
-                            <label class="response-field-label">字段值: </label>
-                            <input class="input-data response-field-input" id='field_value' />
-                            <br/>
-                            <label class="response-field-label">默认值: </label>
-                            <input class="input-data response-field-input" id='field_default' />
-                            <label class="response-field-label">前处理方法: </label>
-                            <input class="input-data response-field-input" id='field_before_process' />
-                            <label class="response-field-label">后处理方法: </label>
-                            <td class="response-content">
-                            <select class="input-data response-select" id="field_after_process" aria-label="after_process">
-                            <option value="str_remove_by_regex">移除字符串</option>
-                            <option value="str_replace_by_regex">替换字符串</option>
-                            <option value="str_extract_by_regex">提取字符串</option>
-                            <option value="html_removetag_by_xpath">通过xpath移除html标签</option>
-                            <option value="html_removestyle_by_xpath">通过xpath移除html标签的style属性</option>
-                            <option value="html_replacetag_by_xpath"> 通过xpath替换html标签</option>
-                            <option value="format_value"> 格式化字符串</option>
-                            </select>
-                            </td>
-                            <br/>
-                            <label class="response-field-label">字段长度: </label>
-                            <input class="input-data response-field-input" id='field_save_length' />
-                            <label class="response-field-label">字段保存方法: </label>
-                            <td class="response-content">
-                            <select class="input-data response-select" id="field_save_method" aria-label="save_method">
-                            <option value="replace">替换原本的同名字段</option>
-                            <option value="append">追加到原本的同名字段</option>
-                            <option value="add">默认新增字段</option>
-                            </select>
-                            </td>
+                            <input class="input-data response-field-input" id='field_type' /> &emsp;
+                            <label class="response-field-label">长度: </label>
+                            <select class="input-data response-select" id="field_save_length" aria-label="field_save_length">
+                                <option value="0">0</option>
+                                <option value="1">1</option>
+                            </select> &emsp;
                             <img class="response-icon-del" src="./static/pic/del.png" onclick="deleteElement('${tmpId}')">
                             <br />
+
+                            <label class="response-field-label">字段值: </label>
+                            <input class="input-data response-field-input" id='field_value' /> &emsp;
+                            <label class="response-field-label">默认值: </label>
+                            <input class="input-data response-field-input" id='field_default' /> &emsp;
+                            <label class="response-field-label">保存方法: </label>
+                            <select class="input-data response-select" id="field_save_method" aria-label="field_save_method">
+                                <option value="replace">替换原本的同名字段</option>
+                                <option value="append">追加到原本的同名字段</option>
+                                <option value="add">默认新增字段</option>
+                            </select> &emsp;
+                            <br />
+
+                            <label class="response-field-label">后处理方法: </label>
+                            <img class="response-icon-add" src="./static/pic/add2.png" onclick="addAfterProcessList('after_process-${tmpId}')">
+                            <div class="response-process-list" id="after_process-${tmpId}"></div>
                         </div>`;
             area.insertAdjacentHTML('beforeend', temp);
             break;
@@ -58,7 +48,6 @@ function addInputArea(id) {
         case 'save_fields-list': {
             let area = document.getElementById(id);
             console.log("add: save_fields-list")
-            // 随机8位id 0-9 a-z A-Z
             let tmpId = randomId('save-');
             let temp = `<span id="${tmpId}">
                             <input class="input-data response-save-input" />
@@ -68,6 +57,18 @@ function addInputArea(id) {
             break;
         }
     }
+}
+
+/**
+ * 添加解析后处理列表
+ * @param id
+ */
+function addAfterProcessList(id) {
+    let area = document.getElementById(id);
+    console.log("add: " + id)
+    let tmpId = randomId('after_process-');
+    let temp = `后处理列表模板`;
+    area.insertAdjacentHTML('beforeend', temp);
 }
 
 /**
