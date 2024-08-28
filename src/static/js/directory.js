cdda1b6ba33ed6db515b9dd98e8a6acf17cd75f3
hldqe <2114403794@qq.com>
2024-08-28 19:00:17 +0800

    update: 修改目录页面

--- src/static/js/directory.js
+++ src/static/js/directory.js
@@ -27,20 +27,32 @@ function qiehuan(id) {
         nowstep = id;
     }
 }
-// 添加函数
-let tmpdata = 0;
-function addList() {
-    let tmpId=randomId('directory-');
-    let List = document.getElementById("directoryList");
-    
-    let newList = document.createElement("li");
-    newList.setAttribute("id",tmpId);
-    newList.setAttribute("onclick","qiehuan(this.id)");
+// // 添加函数
+// let tmpdata = 0;
+// function addList() {
+//     let tmpId=randomId('directory-');
+//     let List = document.getElementById("directoryList");
 
-    newList.textContent = tmpdata;
-    tmpdata++;
-    List.appendChild(newList);
+//     let newList = document.createElement("li");
+//     newList.setAttribute("id",tmpId);
+//     newList.setAttribute("onclick","qiehuan(this.id)");
 
-
-
+//     newList.textContent = tmpdata;
+//     tmpdata++;
+//     List.appendChild(newList);
+// }
+let number = 0;
+function addList(id) {
+    let area = document.getElementById(id);
+    let tmpId = randomId('directory-');
+    let temp = `<li id="${tmpId}">
+                    <span onclick="qiehuan('${tmpId}')" class="directory-list-left">
+                        阶段-${++number}
+                    </span>
+                    <span class="directory-list-right">
+                        <img src="./static/pic/directoryadd.png" onclick="addList('${tmpId}')" title="点击添加" />
+                        <img src="./static/pic/directoryreduce.png" onclick="deleteElement('${tmpId}')" title="点击删除" />
+                    </span>
+                </li>`;
+    area.insertAdjacentHTML('afterend', temp);
 }
